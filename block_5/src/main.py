0ea46c84af56afc16813a2597e812d6b9c9e5a01
Pavel <pavlovrosatom@gmail.com>
2024-03-15 17:55:41 +0300

    ADD: needed funks for module 5

--- block_5/src/main.py
+++ block_5/src/main.py
@@ -9,9 +9,17 @@ class RequestType(Enum):
 
 
 class ApiRequest:
-    def __init__(self, payload, request_type: RequestType):
+    def __init__(self, payload, request_type):
         self.payload = payload
-        self.request_type = request_type
+        if not hasattr(RequestType, request_type):
+            raise TypeError('Vse ploho deneg net')
+        self.request_type = RequestType()
 
     def set_payload(self, new_payload):
         self.payload = new_payload
+
+    def get_payload(self):
+        return self.payload
+
+    def get_type(self):
+        return self.payload
--- block_5/test/test_main.py
+++ block_5/test/test_main.py
@@ -1,5 +1,31 @@
 import pytest
-from block_5.src.main import *
+from contextlib import nullcontext as ex_not_raise
+from block_5.src.main import ApiRequest
 
 
 class TestApiRequest:
+    @pytest.mark.parametrize(
+        "type, payload, res, ex",
+        ('GET', 'payload', 'GET', ex_not_raise()),
+        ('Get', 'payload', 'Get', pytest.raises(TypeError)),
+        ('POST', 'hidden payload', 'POST', ex_not_raise()),
+        ('NEPOST', 'hidden payload', 'NEPOST', pytest.raises(TypeError)),
+    )
+    def test_type_ApiRequest(self, type, payload, res, ex):
+        api = ApiRequest()
+        with ex:
+
+
+    @pytest.mark.parametrize(
+        "type, payload, ex",
+        ()
+    )
+    def test_payload_ApiRequest(self):
+        pass
+
+    @pytest.mark.parametrize(
+        "type, payload, ex",
+        ()
+    )
+    def test_set_payload_ApiRequest(self):
+        pass
