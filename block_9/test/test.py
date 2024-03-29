a4ea31e314ec4b46483480d9f3b106fe7cf16929
relax fm <arton.2@mail.ru>
2024-03-29 18:01:18 +0300

    ADD: files for module 9

--- block_9/test/test.py
+++ block_9/test/test.py
@@ -40,41 +40,90 @@ class Test_Saucedemo:
             time.sleep(0.4)
             password_input.send_keys(Keys.ENTER)
             time.sleep(1)
+            assert "https://www.saucedemo.com/inventory.html" in driver.current_url
         except Exception as ex:
             print(ex)
             driver.close()
             driver.quit()
 
+    def logout(self, driver):
+        try:
+            driver.find_element(By.ID, 'react-burger-menu-btn').click()
+            time.sleep(0.4)
+            driver.find_element(By.ID, 'logout_sidebar_link').click()
+            time.sleep(1)
+            assert "https://www.saucedemo.com" in driver.current_url
+        except Exception as ex:
+            print(ex)
+            driver.close()
+            driver.quit()
 
     @pytest.mark.parametrize(
         ["login", "password"],
         [
             ("standard_user", "secret_sauce"),
-            ("locked_out_user", "secret_sauce"),
             ("problem_user", "secret_sauce"),
             ("performance_glitch_user", "secret_sauce"),
             ("error_user", "secret_sauce"),
             ("visual_user", "secret_sauce")
         ]
     )
-    def test_login(self, login, password):
+    def test_login_logout(self, login, password):
         try:
             driver = self.driver()
             self.login(driver, login, password)
+            self.logout(driver)
         except Exception as ex:
             print(ex)
         finally:
             driver.close()
             driver.quit()
 
+    def buy(self, driver, id):
+        try:
+            driver.find_element(By.ID, id).click()
+            time.sleep(0.4)
+            driver.find_element(By.CLASS_NAME, 'shopping_cart_link').click()
+            time.sleep(0.4)
+            driver.find_element(By.ID, 'checkout').click()
+            element = driver.find_element(By.ID, 'first-name')
+            element.send_keys("Relax")
+            time.sleep(0.4)
+            element = driver.find_element(By.ID, 'last-name')
+            element.send_keys("Fm")
+            time.sleep(0.4)
+            element = driver.find_element(By.ID, 'postal-code')
+            element.send_keys("1234")
+            time.sleep(0.4)
+            driver.find_element(By.ID, 'continue').click()
+            time.sleep(0.4)
+            driver.find_element(By.ID, 'finish').click()
+            time.sleep(0.4)
+            assert "https://www.saucedemo.com/checkout-complete.html" in driver.current_url
+        except Exception as ex:
+            print(ex)
+            driver.close()
+            driver.quit()
 
-    def test_log(self, log_in):
-        # try:
-        #     driver = self.driver()
-        #
-        # except Exception as ex:
-        #     print(ex)
-        # finally:
-        #     driver.close()
-        #     driver.quit()
-        assert 1 == 1
+    @pytest.mark.parametrize(
+        "id",
+        [
+            "add-to-cart-sauce-labs-backpack",
+            "add-to-cart-sauce-labs-bike-light",
+            "add-to-cart-sauce-labs-bolt-t-shirt",
+            "add-to-cart-sauce-labs-fleece-jacket",
+            "add-to-cart-sauce-labs-onesie",
+            "add-to-cart-test.allthethings()-t-shirt-(red)",
+        ]
+    )
+    def test_buy(self, id):
+        try:
+            driver = self.driver()
+            self.login(driver, "standard_user", "secret_sauce")
+            self.buy(driver, id)
+            self.logout(driver)
+        except Exception as ex:
+            print(ex)
+        finally:
+            driver.close()
+            driver.quit()
